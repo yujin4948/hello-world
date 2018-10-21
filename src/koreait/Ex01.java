32d9124a0dc51e8b53d47e36d1c8f407a069ab19
yang <yang@DESKTOP-90TF9QG>
2018-10-22 00:07:04 +0900

    initial commit

---
+++ src/koreait/Ex01.java
@@ -0,0 +1,36 @@
+package koreait;
+
+// 한 줄 주석 : 패키지 - 동일한 클래스들의 모임 (폴더와 유사)
+
+/*
+ * 두 줄 이상 주석
+ * 
+ * 작성자 : 양유진
+ * 작성일 : 2018-06-28
+ * 내  용 : java 기초
+ */
+
+public class Ex01 { // Ex01 클래스 시작
+
+	public static void main(String[] arg) { // main() 메서드 시작
+		
+		System.out.println("자바 프로그램에 오신 여러분을 환영합니다.");
+		System.out.print("반갑습니다.");
+		System.out.print("지금 비가 오네요.");
+		System.out.println();
+		System.out.println("오늘은 날씨가 덥네요...");
+		
+		/*
+		 * System.out : 표준출력장치(모니터, 콘솔)
+		 * println() : 지정된 장치에 결과를 출력하는 메서드
+		 *             => ln: line(출력 후 줄바꿈)
+		 * print() : 다음 줄로 이동하지 않고 옆(오른쪽)에 출력
+		 */
+		
+		/*
+		 * 이클립스 출력 관련 단축키
+		 * syso + Ctrl + space ==> 출력문 단축키
+		 */
+	} // main() 메서드 끝
+	
+} // Ex01 클래스 끝
---
+++ src/koreait/Ex02.java
@@ -0,0 +1,21 @@
+package koreait;
+
+/*
+ * [문제] 아래와 같이 콘솔 창에 출력하세요.
+ * 		이름 : 양유진
+ * 		연락처 : 010-1234-1234
+ * 		이메일 : yu@naver.com
+ * 		주소 : 경기도 고양시
+ */
+public class Ex02 {
+
+	public static void main(String[] args) {
+		System.out.println("이름 : 양유진");
+		System.out.println("연락처 : 010-1234-1234");
+		System.out.println("이메일 : yu@naver.com");
+		System.out.println("주소 : 경기도 고양시");
+		
+		// 실행 단축키 : Ctrl + F11
+	}
+	
+}
